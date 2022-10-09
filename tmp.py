d3aed7733202fcca9a0bdfbaa1eb0201a6c894ff
carbonscott <wangimagine@gmail.com>
2022-10-08 22:09:40 -0700

    Update examples.

--- tmp.py
+++ tmp.py
@@ -5,26 +5,47 @@ from toygrad.scalar import Scalar
 
 import torch
 
-a1 = Scalar(2.0)
-a2 = Scalar(3.0)
+from graphviz import Digraph
 
-t1 = a1 * a2
-t2 = a1 * a1
-t3 = t1 + t2
-t4 = t3 + a2
+def trace(root):
+  # builds a set of all nodes and edges in a graph
+  nodes, edges = list(), list()
+  def build(v):
+    if v not in nodes:
+      ## nodes.add(v)
+      nodes.append(v)
+      for child in v.prev_list:
+        ## edges.add((child, v))
+        edges.append((child, v))
+        build(child)
+  build(root)
+  return nodes, edges
 
-t4.backward()
+def draw_dot(root):
+  dot = Digraph(format='pdf', graph_attr={'rankdir': 'LR'}, filename = 'test.gv') # LR = left to right
 
-b1 = torch.tensor(2.0, requires_grad = True)
-b2 = torch.tensor(3.0, requires_grad = True)
+  ## nodes, edges = trace(root)
+  nodes, edges = root.build_graph()
+  for n in nodes:
+    uid = str(id(n))
+    # for any value in the graph, create a rectangular ('record') node for it
+    dot.node(name = uid, label = "{ %s | value %.4f | grad %.4f }" % (n.label, n.value, n.grad), shape='record')
+    if n._op:
+      # if this value is a result of some operation, create an op node for it
+      dot.node(name = uid + n._op, label = n._op)
+      # and connect this node to it
+      dot.edge(uid + n._op, uid)
 
-s1 = b1 * b2
-s2 = b1 * b1
-s3 = s1 + s2
+  for n1, n2 in edges:
+    # connect n1 to the op node of n2
+    dot.edge(str(id(n1)), str(id(n2)) + n2._op)
 
-s2.retain_grad()
-s3.retain_grad()
+  return dot
 
-s4 = s3 + b2
+a1 = Scalar(4.0, 'a1')
+a2 = Scalar(-3.0, 'a2')
+a3 = Scalar(2.0, 'a3')
 
-s4.backward()
+res = a1 * a2 + a2 * a3
+res.backward()
+draw_dot(res).render()
